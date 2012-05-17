dd8725ad281b07d80aa3d847a53536ddf4894eeb
Michele Tavella <michele.tavella@epfl.ch>
2012-05-17 14:48:48 +0200

    Added initial VS2010 solution

---
+++ misc/tobicore/tobicore/tobicore.h
@@ -0,0 +1,13 @@
+// tobicore.h
+
+#pragma once
+
+using namespace System;
+
+namespace tobicore {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ misc/tobicore/tobiid/tobiid.h
@@ -0,0 +1,13 @@
+// tobiid.h
+
+#pragma once
+
+using namespace System;
+
+namespace tobiid {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
